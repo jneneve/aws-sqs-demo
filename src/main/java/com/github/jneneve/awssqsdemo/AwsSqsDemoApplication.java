7e5b808482ff1628659ddfd4926f36a021e2cf25
Josue da Cruz <jdacruz@wiley.com>
2025-01-20 08:56:22 -0300

    Add the Docker Compose configuration to the LocalStack container and include an initialization file to create the AWS services

---
+++ src/main/java/com/github/jneneve/awssqsdemo/AwsSqsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.jneneve.awssqsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSqsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSqsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jneneve/awssqsdemo/AwsSqsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jneneve.awssqsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
